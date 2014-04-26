4054669d18b23cc457ad19bdc70e8d281f3fa2a7
Pat Squires <pdasquires@hotmail.co.uk>
2014-04-26 16:59:11 +0100

    Client re-structured
    The client class has been restructured to use methods, rather than one
    large main method.

--- BTC/src/svc/Client.java
+++ BTC/src/svc/Client.java
@@ -6,59 +6,62 @@ import points.Point3d;
 
 
 public class Client {
+	
+	private Socket socket = null;
+	private ObjectOutputStream out = null;
+	private ObjectInputStream in = null;
 
+	public Client() throws IOException {
+		 
+		try {
+			// echoSocket = new Socket("taranis", 7);
+	        socket = new Socket("127.0.0.1", 10007);
+
+	        } catch (UnknownHostException e) {
+	            System.err.println("Don't know about host: taranis.");
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.err.println("Couldn't get I/O for "
+	                               + "the connection to: taranis.");
+	            System.exit(1);
+	        }    
+		out = new ObjectOutputStream(socket.getOutputStream());
+		in = new ObjectInputStream(socket.getInputStream());
+	}
+	
+	public void setObjectOutStream(Object obj) throws IOException {
+		
+		out.writeObject(obj);
+        out.flush();
+	}
+	
+	public void cancelClient() throws IOException{
+		out.close();
+		in.close();
+		socket.close();
+	}
+	
 	public static void main(String[] args) throws IOException {
 
-        Socket echoSocket = null;
-        ObjectOutputStream out = null;
-        ObjectInputStream in = null;
+		//Point3d pt1 = new Point3d (7, 8, 10);
+        //Point3d pt2 = null;
 
-        try {
-            // echoSocket = new Socket("taranis", 7);
-            echoSocket = new Socket("127.0.0.1", 10007);
-            
-            out = new ObjectOutputStream(echoSocket.getOutputStream());
-            in = new ObjectInputStream(echoSocket.getInputStream());
+        //System.out.println ("Sending point: " + pt1 + " to Server");
+        
+        //System.out.println ("Send point, waiting for return value");
 
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: taranis.");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for "
-                               + "the connection to: taranis.");
-            System.exit(1);
-        }
+        //try {
+        //     pt2 = (Point3d) in.readObject();
+        //    }
+        // catch (Exception ex)
+        //    {
+        //    System.out.println (ex.getMessage());
+        //    }
 
-
-
-	Point3d pt1 = new Point3d (7, 8, 10);
-        Point3d pt2 = null;
-
-        System.out.println ("Sending point: " + pt1 + " to Server");
-	out.writeObject(pt1);
-
-    
-    
-    
+        //System.out.println("Got point: " + pt2 + " from Server");
 	
-        out.flush();
-        System.out.println ("Send point, waiting for return value");
+        //System.console().readLine();
 
-        try {
-             pt2 = (Point3d) in.readObject();
-            }
-        catch (Exception ex)
-            {
-             System.out.println (ex.getMessage());
-            }
-
-	System.out.println("Got point: " + pt2 + " from Server");
-	
-	System.console().readLine();
-	
-	out.close();
-	in.close();
-	echoSocket.close();
     }
 	
 }
