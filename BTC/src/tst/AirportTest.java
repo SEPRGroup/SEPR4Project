204919dc968247d1c58bd57c59bafe915ae8731f
Stephen Jenkins <sj771@york.ac.uk>
2014-05-05 11:49:31 +0100

    All tests up to date and working
    - AircraftTest now doesn't need testOutOfAirspaceBounds()
    + wrote test method for testIsAt
    + also updated instance for a new Score
    ScoreTest again updated instance for new score
    + needed to include GameWindow class in order to initiate the scoring
    system
    + also needed to initiate the graphics in order to run the test
    - In demo class added a return Game method specific for testing
    + needed it in order to get the GameWindow for the ScoringTest
    - Airport test had to initialise the graphics in order for it to work
    + updated methods that needed extra parameters

--- BTC/src/tst/AirportTest.java
+++ BTC/src/tst/AirportTest.java
@@ -2,8 +2,10 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.Before;
+import org.lwjgl.LWJGLException;
 
 import cls.Aircraft;
 import cls.Airport;
@@ -17,6 +19,13 @@ public class AirportTest {
 	
 	@Before
 	public void setUp() {
+		
+		// Initialise graphics to prevent not loaded images
+		try {
+			org.lwjgl.opengl.Display.create();
+			lib.jog.graphics.initialise();
+		} catch (LWJGLException e) {e.printStackTrace();}
+		
 		test_airport = new Airport("",500,600);
 		Waypoint[] waypoint_list = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
 		test_aircraft = new Aircraft("testAircraft", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypoint_list, 1);			
@@ -56,17 +65,17 @@ public class AirportTest {
 	@Test
 	public void testAddToHangar() {
 		// Dependent on hangar_size 3
-		test_airport.addToHangar(test_aircraft);
+		test_airport.addToHangar(test_aircraft, 1);
 		assertTrue("The size of the hanger = 1", test_airport.aircraft_hangar.size() == 1);
 		
-		test_airport.addToHangar(test_aircraft);
+		test_airport.addToHangar(test_aircraft, 1);
 		assertTrue("The size of the hanger = 2", test_airport.aircraft_hangar.size() == 2);
 		
-		test_airport.addToHangar(test_aircraft);
+		test_airport.addToHangar(test_aircraft, 1);
 		assertTrue("The size of the hanger = 3", test_airport.aircraft_hangar.size() == 3);
 		
 		// This should also be 3 because of the maximum size
-		test_airport.addToHangar(test_aircraft);
+		test_airport.addToHangar(test_aircraft, 1);
 		assertTrue("The size of the hanger = 3", test_airport.aircraft_hangar.size() == 3);
 	}
 	
@@ -75,8 +84,15 @@ public class AirportTest {
 		test_airport.signalTakeOffTesting();
 		assertTrue("The size of the hanger = 0", test_airport.aircraft_hangar.size() == 0);
 		
-		test_airport.addToHangar(test_aircraft);
+		test_airport.addToHangar(test_aircraft, 1);
 		test_airport.signalTakeOffTesting();
 		assertTrue("The size of the hanger = 0", test_airport.aircraft_hangar.size() == 0);
+	}
+	
+	@After
+	public void tearDown() {
+		
+		// cannot have more than one window open
+		org.lwjgl.opengl.Display.destroy();
 	}
 }
