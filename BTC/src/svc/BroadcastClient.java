59c21e97ead19c6b6e5f8f7c21ff2288eacb14cf
cjf521 <cjf521@york.ac.uk>
2014-05-05 15:04:22 +0100

    BroadcastClient extended
    BroadcastClient:
    -qualified request code (authentication!)
    -commented block that broadcast over all network devices
    +in tests, the default broadcast has performed and this removed defaults
    -modifed to receive as many responses as arrive before a 1 second
    timeout
    -modified to receive additional data in the response packet
    -added BroadcastResponse to store responses for transfer

--- BTC/src/svc/BroadcastClient.java
+++ BTC/src/svc/BroadcastClient.java
@@ -2,8 +2,8 @@ package svc;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Enumeration;
-
 
 public class BroadcastClient implements Runnable {
 	DatagramSocket c;
@@ -13,15 +13,26 @@ public class BroadcastClient implements Runnable {
 		client.run();
 	}
 
+
 	@Override
 	public void run() {
+		/** based off original code
+		/***************************************************************************************
+		*    Title: Network discovery using UDP Broadcast (Java)
+		*    Author: Michiel De Mey 
+		*    Date: 2014/05/05
+		*    Code version: N/A
+		*    Availability: http://michieldemey.be/blog/network-discovery-using-udp-broadcast/
+		*
+		***************************************************************************************/
+		
 		// Find the server using UDP broadcast
 		try {
 			//Open a random port to send the package
 			c = new DatagramSocket();
 			c.setBroadcast(true);
 
-			byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
+			byte[] sendData = "SEPR_PSA_REQUEST_54321".getBytes();
 
 			//Try the 255.255.255.255 first
 			try {
@@ -31,7 +42,7 @@ public class BroadcastClient implements Runnable {
 			} catch (Exception e) {
 			}
 
-			// Broadcast the message over all the network interfaces
+			/*// Broadcast the message over all the network interfaces
 			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 			while (interfaces.hasMoreElements()) {
 				NetworkInterface networkInterface = interfaces.nextElement();
@@ -58,27 +69,63 @@ public class BroadcastClient implements Runnable {
 			}
 
 			System.out.println(getClass().getName() + ">>> Done looping over all network interfaces. Now waiting for a reply!");
+			*/
 
 			//Wait for a response
-			byte[] recvBuf = new byte[15000];
-			DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-			c.receive(receivePacket);
-
-			//We have a response
-			System.out.println(getClass().getName() + ">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
-
-			//Check if the message is correct
-			String message = new String(receivePacket.getData()).trim();
-			if (message.equals("DISCOVER_FUIFSERVER_RESPONSE")) {
-				//DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
-				System.out.println(receivePacket.getAddress());
+			ArrayList<DatagramPacket> responses = new ArrayList<DatagramPacket>(); 
+			c.setSoTimeout(1000);
+			while (true){
+				try{
+					byte[] recvBuf = new byte[15000];
+					DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+					c.receive(receivePacket);
+					responses.add(receivePacket);
+				}
+				catch (java.net.SocketTimeoutException e){
+					break;
+				}
 			}
-
 			//Close the port!
 			c.close();
+
+			//We have a response?
+			ArrayList<BroadcastResponse> pResponses = new ArrayList<BroadcastResponse>();
+			for (DatagramPacket r : responses){
+				System.out.println(getClass().getName() + ">>> Broadcast response from server: " + r.getAddress().getHostAddress());
+				//Check if the message is correct
+				String message = new String(r.getData()).trim();
+				String[] data = message.split("@");
+				if ((data.length > 0) && data[0].equals("SEPR_PSA_VALIDRESPONSE_12345")) {
+					//DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
+					pResponses.add(new BroadcastResponse(r.getAddress(), data));
+				}
+			}
+			
+			for (BroadcastResponse r: pResponses){
+				System.out.print(r.responder +":\t");
+				for (String s: r.response){
+					System.out.print(s +", ");
+				}
+				System.out.println();
+			}
+			
+
+
 		} catch (IOException ex) {
 
 		}
+	}
+	
+	private class BroadcastResponse{
+		
+		public final InetAddress responder;
+		public final String[] response;
+		
+		private BroadcastResponse(InetAddress responder, String[] response) {
+			this.responder = responder;
+			this.response = response;
+		}
+		
 	}
 }
 
